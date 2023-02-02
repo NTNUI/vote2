8f27edddfcfe791794098c64192f3dc7c679bd1b
Eivind Dalholt <eivind.dalholt@gmail.com>
2023-02-02 13:22:20 +0100

    Feat: auth routes for login/logout, token refresh and token verify (#18)
    * feat: auth routes for login, token refresh and token verify
    
    * authorizationMiddleware for auth
    
    * refactor: types/enum for roles and groups
    
    * fix: mongoversion

---
+++ backend/types/user.ts
@@ -0,0 +1,13 @@
+import { userRole } from "../enums/userRole";
+
+export type UserType = {
+  _id: number;
+  first_name: string;
+  last_name: string;
+  groups: GroupType[];
+};
+
+export type GroupType = {
+  groupName: string;
+  role: userRole;
+};
