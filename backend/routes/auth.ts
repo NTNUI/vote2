fb161de99543f8483121fb656ac5eec7c82d7031
Eivind Dalholt <eividalh@stud.ntnu.no>
2023-02-02 16:38:42 +0100

    merge main into branch

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -1,19 +1,12 @@
 import { Router } from "express";
 import { login, logout } from "../controllers/auth";
 
-<<<<<<< HEAD
-const authRoutes = express.Router();
-=======
 const authRoutes = Router();
->>>>>>> main
 
 // Login route
 // Return access and refresh tokens
 authRoutes.get("/login", login);
 
-<<<<<<< HEAD
-=======
 authRoutes.get("/logout", logout);
 
->>>>>>> main
 export default authRoutes;
