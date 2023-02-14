e9805a8a354f6d2f214146554d7d3a73341060e4
Eivind Dalholt <eividalh@stud.ntnu.no>
2023-02-14 12:30:48 +0100

    feat: assembly edit and delete endpoints

--- backend/controllers/assembly.ts
+++ backend/controllers/assembly.ts
@@ -2,7 +2,7 @@ import { Response } from "express";
 import { Assembly } from "../models/assembly";
 import { User } from "../models/user";
 import { RequestWithNtnuiNo } from "../utils/request";
-import { isGroupOrganizer } from "./user";
+import { isGroupOrganizer } from "../utils/user";
 
 export async function createAssembly(req: RequestWithNtnuiNo, res: Response) {
   if (!req.ntnuiNo) {
@@ -20,14 +20,66 @@ export async function createAssembly(req: RequestWithNtnuiNo, res: Response) {
     ) {
       await Assembly.findByIdAndUpdate(
         group,
-        { $set: { isActive: false, participants: 0 } },
+        { $set: { isActive: false, participants: 0, createdBy: Number(req.ntnuiNo) } },
         { upsert: true }
       );
-      return res.status(200).json({ message: "Assembly succefully created" });
+      return res.status(200).json({ message: "Assembly successfully created" });
     }
   }
 
   return res
     .status(401)
     .json({ message: "You are not authorized to create this assembly" });
+}
+
+export async function editAssembly(req: RequestWithNtnuiNo, res: Response) {
+  if (!req.ntnuiNo) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const group = req.body.group;
+  const user = await User.findById(req.ntnuiNo);
+
+  if (user) {
+    if (
+      user.groups.some(
+        (membership) =>
+          isGroupOrganizer(membership) && membership.groupName == group
+      )
+    ) {
+      await Assembly.findByIdAndUpdate(
+        group,
+        { $set: { isActive: req.body.isActive, participants: 0 } },
+        { upsert: true }
+      );
+      return res.status(200).json({ message: "Assembly successfully updated" });
+    }
+  }
+
+  return res
+    .status(401)
+    .json({ message: "You are not authorized to edit this assembly" });
+}
+
+export async function deleteAssembly(req: RequestWithNtnuiNo, res: Response) {
+  if (!req.ntnuiNo) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const group = req.body.group;
+  const user = await User.findById(req.ntnuiNo);
+
+  if (user) {
+    if (
+      user.groups.some(
+        (membership) =>
+          isGroupOrganizer(membership) && membership.groupName == group
+      )
+    ) {
+      await Assembly.findByIdAndRemove(group);
+      return res.status(200).json({ message: "Assembly successfully deleted" });
+    }
+  }
+
+  return res
+    .status(401)
+    .json({ message: "You are not authorized to delete this assembly" });
 }
--- backend/routes/assembly.ts
+++ backend/routes/assembly.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { createAssembly } from "../controllers/assembly";
+import { createAssembly, deleteAssembly, editAssembly } from "../controllers/assembly";
 import authorization from "../utils/authorizationMiddleware";
 
 const assemblyRoutes = Router();
 
 assemblyRoutes.post("/", authorization, createAssembly);
+
+assemblyRoutes.put("/", authorization, editAssembly);
+
+assemblyRoutes.delete("/", authorization, deleteAssembly);
 
 export default assemblyRoutes;
