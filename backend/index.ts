2aac8cc9b6c06c8befb3073fe00bcbbdb0811c07
Eivind Dalholt <eividalh@stud.ntnu.no>
2023-02-14 11:18:27 +0100

    format: prettier

--- backend/index.ts
+++ backend/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app: Application = express();
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
--- frontend/src/styles/groupStyles.ts
+++ frontend/src/styles/groupStyles.ts
@@ -1,6 +1,6 @@
 import { createStyles } from "@mantine/core";
 
-export const useStyles = createStyles((theme) => ({
+export const useStyles = createStyles(() => ({
   activeBox: {
     color: "white",
     minWidth: 250,
