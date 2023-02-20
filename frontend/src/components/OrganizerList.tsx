8334c348b01facab44a02a663436db1a0a676367
Eivind Dalholt <eivind.dalholt@gmail.com>
2023-02-20 13:09:41 +0100

    fix: simplified logic for fetching groups, avoiding duplicates (#53)
    * fix: simplified logic for fetching groups, avoiding duplicates
    
    * fix: change from grid to flexbox on group boxes
    
    * fix: prettier
    
    * refactor: separate component for group organizer box

--- frontend/src/components/OrganizerList.tsx
+++ frontend/src/components/OrganizerList.tsx
@@ -1,18 +1,16 @@
-import { Box, Button, Flex, SimpleGrid, Space } from "@mantine/core";
+import { SimpleGrid } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getGroups } from "../services/organizer";
+import { getUserData } from "../services/organizer";
 import Arrow from "../assets/Arrow.svg";
 import { useMediaQuery } from "@mantine/hooks";
-
-interface GroupData {
-  groupName: string;
-  role: string;
-  hasActiveAssembly: boolean;
-}
+import { UserDataGroupType } from "../types/user";
+import { OrganizerGroupBox } from "./OrganizerGroupBox";
 
 export function OrganizerList() {
-  const [organizedGroups, setOrganizedGroups] = useState<GroupData[]>([]);
+  const [organizerGroups, setOrganizerGroups] = useState<UserDataGroupType[]>(
+    []
+  );
   let navigate = useNavigate();
   const matches = useMediaQuery("(min-width: 600px)");
 
@@ -28,87 +26,21 @@ export function OrganizerList() {
     navigate("/assembly");
   }
 
-  function createGroupBox(group: GroupData, index: number) {
-    let groupName = group.groupName;
-    groupName = groupName.charAt(0).toUpperCase() + groupName.slice(1);
-    const startCheckinTestID: string = "checkin-button-" + group.groupName;
-    const createAssemblyTestID: string =
-      "create-assembly-button-" + group.groupName + "-" + index;
-    const editAssemblyTestID: string =
-      "edit-assembly-button-" + group.groupName;
-    return (
-      <>
-        <Space h="sm" />
-        <Box
-          key={index}
-          sx={(theme) => ({
-            borderStyle: "solid",
-            borderColor: "white",
-            textAlign: "center",
-            borderRadius: theme.radius.md,
-            color: "white",
-          })}
-        >
-          <Flex
-            mih={50}
-            gap="xl"
-            justify="space-between"
-            align="center"
-            direction="row"
-            wrap="wrap"
-          >
-            <h4 style={{ marginLeft: "2vw" }}>{groupName}</h4>
-            {group.hasActiveAssembly ? (
-              <div style={{ marginRight: "2vw" }}>
-                <Button
-                  color="green"
-                  radius="md"
-                  onClick={handleQRClick}
-                  data-testid={startCheckinTestID}
-                >
-                  Start checkin
-                </Button>
-                <Button
-                  color="gray"
-                  radius="md"
-                  onClick={handleCreateAssemblyClick}
-                  data-testid={editAssemblyTestID}
-                >
-                  Edit
-                </Button>
-              </div>
-            ) : (
-              <Button
-                style={{ marginRight: "2vw" }}
-                onClick={handleCreateAssemblyClick}
-                data-testid={createAssemblyTestID}
-              >
-                Create assembly
-              </Button>
-            )}
-          </Flex>
-        </Box>
-      </>
-    );
-  }
-
-  async function sortGroups() {
+  async function fetchGroups() {
     try {
-      const groupsRequest = await getGroups();
-      const groups = groupsRequest.data.groups;
-      for (let i = 0; i < groups.length; i++) {
-        let group: GroupData = groups[i];
-        if (group.role == "organizer") {
-          setOrganizedGroups((organizedGroups) => [...organizedGroups, group]);
-        }
-      }
+      const groups = (await getUserData()).groups;
+      setOrganizerGroups(
+        groups.filter((group) => {
+          return group.role == "organizer";
+        })
+      );
     } catch (error) {
       console.log(error);
     }
   }
 
   useEffect(() => {
-    sortGroups();
+    fetchGroups();
   }, []);
 
   return (
@@ -127,7 +59,7 @@ export function OrganizerList() {
             GROUPS{" "}
           </p>
           <img src={Arrow}></img>
-          <p style={{ display: "inline", fontWeight: "bold" }}> ORGANIZER</p>
+          <p style={{ display: "inline", fontWeight: "bold" }}>ORGANIZER</p>
         </div>
         <h2
           style={{ display: "flex", alignItems: "center" }}
@@ -138,7 +70,9 @@ export function OrganizerList() {
         </h2>
         <div></div>
       </SimpleGrid>
-      {organizedGroups.map((group, index) => createGroupBox(group, index))}
+      {organizerGroups.map((group, index) => (
+        <OrganizerGroupBox key={index} {...{ group, index: index }} />
+      ))}
     </>
   );
 }
