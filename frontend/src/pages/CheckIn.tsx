196094f7b78ed6272c8ad7da8ba59f64943e3faa
Eivind Dalholt <eividalh@stud.ntnu.no>
2023-03-14 11:10:21 +0100

    feat: qr scanner

--- frontend/src/pages/CheckIn.tsx
+++ frontend/src/pages/CheckIn.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import { Container, Text } from "@mantine/core";
+import { QrScanner } from "@yudiel/react-qr-scanner";
+import { useState } from "react";
 
 export function CheckIn() {
-  return <p>CheckIn</p>;
+  const [result, setResult] = useState("");
+  return (
+    <>
+      <Container>
+        <QrScanner
+          containerStyle={{ width: 500 }}
+          onDecode={(result) => setResult(result)}
+          onError={(error) => console.log(error?.message)}
+        />
+        <Text>{result}</Text>
+      </Container>
+    </>
+  );
 }
