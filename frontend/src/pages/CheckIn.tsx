42c54c69c4cbac3220961f3de978f7a82a2f335a
Eivind Dalholt <eivind.dalholt@gmail.com>
2023-03-14 15:54:19 +0100

    Feat/QR code scanner (#102)
    * feat: qr scanner
    
    * feat: qr scanner error handling and user feedback

--- frontend/src/pages/CheckIn.tsx
+++ frontend/src/pages/CheckIn.tsx
@@ -1,5 +1,64 @@
-import React from "react";
+import { Container, Loader, Text } from "@mantine/core";
+import { QrScanner } from "@yudiel/react-qr-scanner";
+import { useEffect, useState } from "react";
+import { showNotification } from "@mantine/notifications";
+import { assemblyCheckin } from "../services/qr";
+import { QRType } from "../types/checkin";
 
 export function CheckIn() {
-  return <p>CheckIn</p>;
+  const [result, setResult] = useState<string>();
+  const [processing, setProcessing] = useState<boolean>(false);
+
+  const checkInUser = async (data: QRType) => {
+    setProcessing(true);
+    if (await assemblyCheckin(data)) {
+      showNotification({
+        title: "Success",
+        message: "User is checked-in",
+      });
+    } else {
+      showNotification({
+        title: "Error",
+        message: "Failed to check in user",
+      });
+    }
+    setProcessing(false);
+  };
+
+  useEffect(() => {
+    if (result) {
+      try {
+        const decodedResult = JSON.parse(result);
+        const { access, group, timestamp } = decodedResult;
+        checkInUser({ access, group, timestamp });
+      } catch (error) {
+        showNotification({
+          title: "Error",
+          message: "The given QR code is not valid",
+        });
+      }
+    }
+  }, [result]);
+
+  return (
+    <>
+      <Container>
+        {!processing ? (
+          <>
+            <QrScanner
+              constraints={{ facingMode: "environment" }}
+              containerStyle={{ width: 800 }}
+              onDecode={(result) => setResult(result)}
+              onError={(error) => console.log(error?.message)}
+            />
+          </>
+        ) : (
+          <>
+            <Text size={"lg"}>Processing</Text>
+            <Loader />
+          </>
+        )}
+      </Container>
+    </>
+  );
 }
