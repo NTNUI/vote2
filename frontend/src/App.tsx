75baa581327c74ab388b3a8b42e83dae526f92eb
Sara Holm Pedersen <82167885+sarapedersen@users.noreply.github.com>
2023-03-15 11:48:16 +0100

    feat: add 404 page and remove log out button from header if not logged in (#106)
    * feat: add 404 page and remove log out button from header if not logged in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import colors from "./utils/theme";
 import { Login } from "./pages/LoginPage";
 import axios from "axios";
 import { HeaderAction } from "./components/Header";
+import { NotFound } from "./pages/NotFound";
 import { NotificationsProvider } from "@mantine/notifications";
 
 function App() {
@@ -41,6 +42,15 @@ function App() {
       <NotificationsProvider>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route
+            path="*"
+            element={
+              <>
+                <HeaderAction />
+                <NotFound />
+              </>
+            }
+          />
           <Route element={<ProtectRoutes />}>
             <Route
               path="/start"
