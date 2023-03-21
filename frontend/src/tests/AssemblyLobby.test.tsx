4c13d7ba1287419d73382754ee362158412f8948
Are Saltnes Tømmerås <aresto@stud.ntnu.no>
2023-03-21 10:05:28 +0100

    fix: new fix for error only found in github workflow

--- frontend/src/tests/AssemblyLobby.test.tsx
+++ frontend/src/tests/AssemblyLobby.test.tsx
@@ -2,12 +2,15 @@ import { AssemblyLobby } from "../pages/AssemblyPage";
 import { render, screen } from "@testing-library/react";
 import { describe, expect, test, vi, vitest } from "vitest";
 import { BrowserRouter } from "react-router-dom";
+import * as ReactRouterDom from "react-router-dom";
 
 // Basic example test
 describe("Login test", () => {
   test("Should show text", async () => {
     vi.mock("react-router-dom", async () => {
-      const actual = await vi.importActual("react-router-dom");
+      const actual = (await vi.importActual(
+        "react-router-dom"
+      )) as typeof ReactRouterDom;
       return {
         ...actual,
         useLocation: vitest.fn().mockImplementation(() => {
