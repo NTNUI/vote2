cd564b681701eb8ecbfdb33560736bac6f6b5240
miabjo <69513933+miabjo@users.noreply.github.com>
2023-03-21 14:55:45 +0100

    feat: submit votation endpoint (#105)
    * submit votation endpoint started
    
    * feat: finish submit endpoint in backend
    
    * feat: change get one votation to get current votation

--- backend/routes/votation.ts
+++ backend/routes/votation.ts
@@ -6,7 +6,8 @@ import {
   deleteVotation,
   editVotation,
   getAllVotations,
-  getOneVotation,
+  getCurrentVotation,
+  submitVotation,
 } from "../controllers/votation";
 import authorization from "../utils/authorizationMiddleware";
 
@@ -14,7 +15,7 @@ const votationRoutes = Router();
 
 votationRoutes.post("/allvotations", authorization, getAllVotations);
 
-votationRoutes.post("/onevotation", authorization, getOneVotation);
+votationRoutes.post("/currentvotation", authorization, getCurrentVotation);
 
 votationRoutes.post("/create", authorization, createVotation);
 
@@ -25,5 +26,7 @@ votationRoutes.put("/deactivate", authorization, deactivateVotationStatus);
 votationRoutes.delete("/", authorization, deleteVotation);
 
 votationRoutes.put("/", authorization, editVotation);
+
+votationRoutes.put("/submit", authorization, submitVotation);
 
 export default votationRoutes;
