b9c3e0f95ba25fab7d1f4797fd1f81ef5148c4e6
Are Saltnes Tømmerås <aresto@stud.ntnu.no>
2023-03-21 11:26:19 +0100

    feat: added breadcrumbs to checkin-scaner-page and to assemblylobby when not in an assembly

--- frontend/src/pages/CheckIn.tsx
+++ frontend/src/pages/CheckIn.tsx
@@ -1,15 +1,18 @@
-import { Container, Loader, Text } from "@mantine/core";
+import { Box, Image, Container, Loader, Text } from "@mantine/core";
 import { QrScanner } from "@yudiel/react-qr-scanner";
 import { useEffect, useState } from "react";
 import { showNotification } from "@mantine/notifications";
 import { assemblyCheckin } from "../services/qr";
 import { QRType } from "../types/checkin";
 import { useMediaQuery } from "@mantine/hooks";
+import { useNavigate } from "react-router-dom";
+import Arrow from "../assets/Arrow.svg";
 
 export function CheckIn() {
   const [result, setResult] = useState<string>();
   const [processing, setProcessing] = useState<boolean>(false);
   const matches = useMediaQuery("(max-width: 450px)");
+  let navigate = useNavigate();
 
   const checkInUser = async (data: QRType) => {
     setProcessing(true);
@@ -32,9 +35,41 @@ export function CheckIn() {
     }
   }, [result]);
 
+  function handleBreadcrumbGroupClick() {
+    navigate("/start");
+  }
+  function handleBreadcrumbOrganizerClick() {
+    navigate("/admin");
+  }
   return (
     <>
-      <Container>
+      <Box
+        style={{
+          position: "absolute",
+          top: 70,
+          left: 30,
+          display: "flex",
+          alignItems: "center",
+          cursor: "pointer",
+        }}
+      >
+        <Text fz={"sm"} fw={500} onClick={() => handleBreadcrumbGroupClick()}>
+          GROUPS
+        </Text>
+        <Image width={15} m={10} src={Arrow}></Image>
+        <Text
+          fz={"sm"}
+          fw={500}
+          onClick={() => handleBreadcrumbOrganizerClick()}
+        >
+          ORGANIZER
+        </Text>
+        <Image width={15} m={10} src={Arrow}></Image>
+        <Text fz={"sm"} fw={600}>
+          CHECK-IN-SCANNER
+        </Text>
+      </Box>
+      <Container mt={50}>
         {!processing ? (
           <>
             <QrScanner
