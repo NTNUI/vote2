a454121cdecf59544a588a90aebc3c58514f29f1
Magnus Svendsen <magnsven@stud.ntnu.no>
2023-04-19 10:52:14 +0200

    fixed old e2e tests to work with current project

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://localhost:5173",
     setupNodeEvents() {
       // implement node event listeners here
     },
