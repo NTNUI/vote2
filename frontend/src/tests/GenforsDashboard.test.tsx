2ab2326555034d1c5736a5838362b24f005c9ef0
Are Tømmerås <69514365+tommeras@users.noreply.github.com>
2023-04-19 10:15:01 +0200

    test: add unit testing to frontend (#163)
    * added test to startpage and resultpage
    
    * test: finalised example unit tests
    
    * refactor: changed variable name to be less general

--- frontend/src/tests/GenforsDashboard.test.tsx
+++ frontend/src/tests/GenforsDashboard.test.tsx
@@ -1,16 +1,58 @@
 import { Assembly } from "../pages/GenforsDashboard";
-import { render, screen } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
+import { act, render, screen } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
 import { BrowserRouter } from "react-router-dom";
+import { UserDataGroupType } from "../types/user";
+
+import * as ReactRouterDom from "react-router-dom";
+import { EditAssembly } from "../components/EditAssembly";
+import colors from "../utils/theme";
+import { MantineProvider } from "@mantine/core";
 
 // Basic example test
-describe("Login test", () => {
-  test("Should show text", () => {
+describe("Test of AssemblyDashboard", () => {
+  test("Should show text", async () => {
+    const state: UserDataGroupType = {
+      groupName: "Sprint",
+      groupSlug: "sprint",
+      hasAssembly: true,
+      organizer: true,
+      hasActiveAssembly: true,
+      creator: "1",
+    };
     render(
       <BrowserRouter>
-        <Assembly />
+        <MantineProvider
+          theme={{
+            fontFamily: "Poppins, sans-serif",
+            colors: colors,
+            fontSizes: {
+              xs: 10,
+              sm: 12,
+              md: 14,
+              lg: 20,
+              xl: 24,
+            },
+            breakpoints: {
+              xs: 500,
+              sm: 800,
+              md: 1000,
+              lg: 1200,
+              xl: 1400,
+            },
+          }}
+        >
+          <EditAssembly group={state} />
+        </MantineProvider>
       </BrowserRouter>
     );
-    //expect(screen.getByText("Genfors dashboard")).toBeDefined()
+    expect(screen.getByTestId("LoaderIcon")).toBeDefined();
+
+    //Waits for the page to render completely and finish rest-calls
+    await act(async () => {
+      await Promise.resolve(EditAssembly);
+    });
+    expect(screen.getByText("Currently 3 participants")).toBeDefined();
+    expect(screen.getByText("EDIT SPRINT ASSEMBLY")).toBeDefined();
   });
 });
